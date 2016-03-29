339aca95479b0c3bec6f760e19755bdfb927a615
Bjørn Friese <bearfrieze@gmail.com>
2016-03-29 16:58:44 +0200

    Managed to draw something graph like

--- 2016-03-14/bearfrieze/present.py
+++ 2016-03-14/bearfrieze/present.py
@@ -1,6 +1,18 @@
 import json
+import networkx as nx
+import matplotlib.pyplot as plt
+
+N = 10
 
 with open('frequencies.json', 'r') as f:
     frequencies = json.loads(f.read())
     frequencies = sorted(frequencies.items(), key=lambda x: x[1], reverse=True)
-    for frequency in frequencies[:20]: print(frequency)
+    print(frequencies[:10])
+    g = nx.Graph()
+    for name, weight in frequencies[1:10]:
+        g.add_edge(frequencies[0][0], name, weight=weight)
+    pos = nx.spring_layout(g)
+    nx.draw_networkx_edges(g, pos, width=1)
+    nx.draw_networkx_nodes(g, pos, node_size=500)
+    nx.draw_networkx_labels(g, pos, font_size=12, font_family='sans-serif')
+    plt.show()
