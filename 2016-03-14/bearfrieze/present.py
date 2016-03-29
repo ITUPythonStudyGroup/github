8b75dd01ccad272d2f98c6bc139dd3a8c0954f78
Bjørn Friese <bearfrieze@gmail.com>
2016-03-29 17:23:09 +0200

    Edge width corresponds to weights

--- 2016-03-14/bearfrieze/present.py
+++ 2016-03-14/bearfrieze/present.py
@@ -3,16 +3,20 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 N = 10
+WIDTH = 5
 
 with open('frequencies.json', 'r') as f:
     frequencies = json.loads(f.read())
     frequencies = sorted(frequencies.items(), key=lambda x: x[1], reverse=True)
-    print(frequencies[:10])
+    master = frequencies[0]
+    frequencies = frequencies[1:N]
     g = nx.Graph()
-    for name, weight in frequencies[1:10]:
-        g.add_edge(frequencies[0][0], name, weight=weight)
-    pos = nx.spring_layout(g)
-    nx.draw_networkx_edges(g, pos, width=1)
+    max_weight = max(f[1] for f in frequencies)
+    for name, weight in frequencies:
+        g.add_edge(master[0], name, weight=(weight/max_weight) * WIDTH)
+    edgewidth = [ d['weight'] for (u,v,d) in g.edges(data=True)]
+    pos = nx.spring_layout(g, iterations=50)
+    nx.draw_networkx_edges(g, pos, width=edgewidth)
     nx.draw_networkx_nodes(g, pos, node_size=500)
     nx.draw_networkx_labels(g, pos, font_size=12, font_family='sans-serif')
     plt.show()
