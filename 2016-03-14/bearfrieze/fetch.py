d52e22cc13c6e229cb290b7ecdfccd27333cc43e
Bjørn Friese <bearfrieze@gmail.com>
2016-04-11 19:09:57 +0200

    Double iteration collaboration edge explorer

--- 2016-03-14/bearfrieze/fetch.py
+++ 2016-03-14/bearfrieze/fetch.py
@@ -3,8 +3,11 @@ import requests
 
 GHA_URL = 'https://api.github.com'
 GHA_TOKEN = os.environ['GHA_TOKEN']
-GHC_URL = 'https://githubcontributions.io/api'
 USER = sys.argv[1]
+LIMIT = 10
+
+def edge_key(a, b):
+    return ':'.join(sorted([a, b]))
 
 def github(path):
     return requests.get(
@@ -15,21 +18,42 @@ def github(path):
         }
     )
 
-repos = requests.get('%s/user/%s' % (GHC_URL, USER)).json()['repos']
-frequencies = {}
-for repo in repos:
-    request = github('repos/%s/contributors' % repo)
-    if request.status_code != 200:
-        print('Bad repo: %s' % repo)
-        continue
-    contributors = request.json()
-    for contributor in contributors:
-        print(contributor['login'])
-        login = contributor['login']
-        if not login in frequencies:
-            frequencies[login] = 1
-        else:
-            frequencies[login] += 1
+def user_frequencies(user):
+    print('Getting frequencies for %s' % user)
+    repos = github('users/%s/repos?type=all&sort=pushed' % user).json()
+    frequencies = {}
+    for repo in repos[:LIMIT]:
+        request = github('repos/%s/contributors' % repo['full_name'])
+        if request.status_code != 200:
+            print('Bad repo: %s' % repo['name'])
+            continue
+        contributors = request.json()
+        for contributor in contributors:
+            print(contributor['login'])
+            login = contributor['login']
+            if not login in frequencies:
+                frequencies[login] = 1
+            else:
+                frequencies[login] += 1
+    if user in frequencies: del frequencies[user]
+    return frequencies
 
-with open('frequencies.json', 'w+') as f:
+frequencies = {}
+fs = user_frequencies(USER)
+frequencies[USER] = fs
+logins_sorted = [t[0] for t in sorted(fs.items(), key=lambda t: t[1], reverse=True)]
+for login in logins_sorted[:min(LIMIT, len(logins_sorted))]:
+    frequencies[login] = user_frequencies(login)
+
+edges = {}
+for a in frequencies.keys():
+    for b in frequencies[a]:
+        key = edge_key(a, b)
+        if not key in edges: edges[key] = 0
+        edges[key] += frequencies[a][b]
+
+with open('%s-frequencies.json' % USER, 'w+') as f:
     f.write(json.dumps(frequencies, indent=2))
+
+with open('%s-edges.json' % USER, 'w+') as f:
+    f.write(json.dumps(edges, indent=2))
